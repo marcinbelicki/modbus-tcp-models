bcd55b3d952b36be0369a6d6ff519186970e1abd
Marcin Belicki <marcin.belicki@protonmail.com>
2026-06-07 15:59:51 +0200

    refactoring

---
+++ modules/models/src/main/scala/pl/belicki/modbus/models/function/ModbusFunction.scala
@@ -0,0 +1,24 @@
+package pl.belicki.modbus.models.function
+
+abstract class ModbusFunction(_code: Int) {
+  val code: Byte = _code.toByte
+
+  abstract class Request {
+    val function: ModbusFunction = ModbusFunction.this
+  }
+
+  abstract class Response {
+    val function: ModbusFunction = ModbusFunction.this
+  }
+
+  case class Error(exceptionCode: Byte) {
+    val functionCode: Byte = Error.code
+  }
+
+  object Error {
+    val code: Byte = (ModbusFunction.this.code + 0x80).toByte
+  }
+
+  type REQ <: Request
+  type RES <: Response
+}
